0449598acb31e9faf459e83f2c27661e11e52bf4
jonasdasneves <jonas.joliveira2024@gmail.com>
2025-04-02 16:39:27 -0300

    Criação de GET e POST, integração com oracle

--- src/main/java/pixelknights/com/tamagochi/resource/TamagochiResource.java
+++ src/main/java/pixelknights/com/tamagochi/resource/TamagochiResource.java
@@ -1,4 +1,48 @@
 package pixelknights.com.tamagochi.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pixelknights.com.tamagochi.model.Tamagochi;
+import pixelknights.com.tamagochi.service.TamagochiService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tamagochi")
 public class TamagochiResource {
+
+    @Autowired
+    private TamagochiService tamagochiService;
+
+    @PostMapping// POST http://localhost:8080/api/tamagochi
+    public ResponseEntity<Tamagochi> save(@RequestBody Tamagochi tamagochi){
+
+        Tamagochi newTamagochi = tamagochiService.save(tamagochi);
+
+        return ResponseEntity.ok(newTamagochi);
+    }
+
+    @GetMapping("/{id}")// GET http://localhost:8080/api/tamagochi/{id}
+    public ResponseEntity<Tamagochi> findById(@PathVariable Long id){
+
+        Tamagochi tamagochi = tamagochiService.findById(id);
+
+        return ResponseEntity.ok(tamagochi);
+    }
+
+    @GetMapping// GET http://localhost:8080/api/tamagochi
+    public  ResponseEntity<List<Tamagochi>> list(){
+
+        List<Tamagochi> tamagochis = tamagochiService.findAll();
+
+        if (tamagochis.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.ok(tamagochis);
+        }
+    }
+
 }
--- src/main/java/pixelknights/com/tamagochi/service/TamagochiService.java
+++ src/main/java/pixelknights/com/tamagochi/service/TamagochiService.java
@@ -1,16 +1,23 @@
 package pixelknights.com.tamagochi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pixelknights.com.tamagochi.exception.BadRequestException;
 import pixelknights.com.tamagochi.exception.InternalServerException;
+import pixelknights.com.tamagochi.exception.NotFoundException;
 import pixelknights.com.tamagochi.model.Tamagochi;
 import pixelknights.com.tamagochi.repository.TamagochiRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TamagochiService {
 
+    @Autowired
     private TamagochiRepository tamagochiRepository;
 
+    //CRUD - Create - Cria um novo bichinho
     public Tamagochi save(Tamagochi tamagochi){
         if (tamagochi.getId() != null){
             throw new BadRequestException("O ID não deve ser passado ao salvar o novo bichinho");
@@ -30,4 +37,26 @@ public class TamagochiService {
             throw new InternalServerException("Erro interno de servidor!");
         }
     }
+
+    //CRUD - Read - Retorna pessoa pelo ID
+    public Tamagochi findById(Long id){
+        try {
+            return tamagochiRepository.findById(id)
+                    .orElseThrow(() -> new NotFoundException("tamagochi com ID " + id + " não encontrado"));
+        } catch (NotFoundException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new InternalServerException("Erro interno ao buscar tamagochi: " + e.getMessage());
+        }
+    }
+
+    //CRUD - Read - Retorna todos os registros de pessoa
+    public List<Tamagochi> findAll(){
+        return tamagochiRepository.findAll();
+    }
+
+
+
+
 }
